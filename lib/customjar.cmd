02eb13dd2c059fa223c1521a19b4060ec16da3e8
kenu <kenu.heo@gmail.com>
2013-03-17 15:09:07 +0900

    register customjar

---
+++ lib/customjar.cmd
@@ -0,0 +1,4 @@
+mvn install:install-file -Dfile=smeapi-2.5-dummy.jar -DgroupId=smeapi -DartifactId=smeapi -Dversion=2.5-dummy -Dpackaging=jar;
+mvn install:install-file -Dfile=gpkisecureweb-1.0.4.9-dummy.jar -DgroupId=egovframework.com.cmm.uat -DartifactId=gpkisecureweb -Dversion=1.0.4.9-dummy -Dpackaging=jar;
+mvn install:install-file -Dfile=libgpkiapi_jni-1.4.0.0-dummy.jar -DgroupId=egovframework.com.cmm.uat -DartifactId=libgpkiapi_jni -Dversion=1.4.0.0-dummy -Dpackaging=jar;
+
